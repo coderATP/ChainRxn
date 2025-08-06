70fb3e2b342391fd193a7428f89cbbce40428cbe
coderATP <atandatosinpete@gmail.com>
2025-08-06 05:46:48 +0100

    update script.js

--- script.js
+++ script.js
@@ -10,71 +10,74 @@ import { OptionsScene } from "./src/scenes/OptionsScene.js";
 import { CreditsScene } from "./src/scenes/CreditsScene.js";
 import { TutorialScene} from "./src/scenes/TutorialScene.js";
 
-let GAME_WIDTH, GAME_HEIGHT, ASPECT_RATIO = innerWidth/innerHeight;
-if(ASPECT_RATIO <= 1 ){
-    GAME_WIDTH = innerWidth*devicePixelRatio;
-    GAME_HEIGHT = innerHeight*devicePixelRatio;
-}
-else{
-    GAME_WIDTH = innerHeight/ ASPECT_RATIO;
-    GAME_HEIGHT = innerHeight;
-}
-const ZOOM_FACTOR = 1;
+window.addEventListener("load", ()=>{
 
-const SHARED_CONFIG = {
-    width: GAME_WIDTH, 
-    height: GAME_HEIGHT,
-    aspectRatio: ASPECT_RATIO,
-    zoomFactor: ZOOM_FACTOR,
-    topLeft: {
-        x: ( GAME_WIDTH - (GAME_WIDTH/ZOOM_FACTOR) ) / 2,
-        y: ( GAME_HEIGHT - (GAME_HEIGHT/ZOOM_FACTOR) ) / 2,
-    },
-    topRight: {
-        x: ( ( GAME_WIDTH - (GAME_WIDTH/ZOOM_FACTOR) ) / 2 ) + (GAME_WIDTH/ZOOM_FACTOR),
-        y: ( GAME_HEIGHT - (GAME_HEIGHT/ZOOM_FACTOR) ) / 2,
-    },
-    bottomRight: {
-        x: ( ( GAME_WIDTH - (GAME_WIDTH/ZOOM_FACTOR) ) / 2 ) + (GAME_WIDTH/ZOOM_FACTOR),
-        y: ( (GAME_HEIGHT - (GAME_HEIGHT/ZOOM_FACTOR) ) / 2 ) + (GAME_HEIGHT/ZOOM_FACTOR),
-    },
-    debug: true
-};
-
-const config= {
-    type: Phaser.CANVAS,
-    ...SHARED_CONFIG, 
-    parent: "gameWrapper",
-    backgroundColor: 0x21190,
-    transparent: true,
-    scale: {
-         mode: Phaser.Scale.Fit,
-         autoCenter: Phaser.Scale.CENTER_BOTH,
-    },
-    pixelArt: false, 
-    physics:{
-        default: 'arcade',
-        arcade:{
-            debug: SHARED_CONFIG.debug,
+    let GAME_WIDTH, GAME_HEIGHT, ASPECT_RATIO = innerWidth/innerHeight;
+    if(ASPECT_RATIO <= 1 ){
+        GAME_WIDTH = innerWidth*devicePixelRatio;
+        GAME_HEIGHT = innerHeight*devicePixelRatio;
+    }
+    else{
+        GAME_WIDTH = innerHeight/ ASPECT_RATIO;
+        GAME_HEIGHT = innerHeight;
+    }
+    const ZOOM_FACTOR = 1;
+    
+    const SHARED_CONFIG = {
+        width: GAME_WIDTH, 
+        height: GAME_HEIGHT,
+        aspectRatio: ASPECT_RATIO,
+        zoomFactor: ZOOM_FACTOR,
+        topLeft: {
+            x: ( GAME_WIDTH - (GAME_WIDTH/ZOOM_FACTOR) ) / 2,
+            y: ( GAME_HEIGHT - (GAME_HEIGHT/ZOOM_FACTOR) ) / 2,
         },
-        matter:{
-            debug: SHARED_CONFIG.debug,
+        topRight: {
+            x: ( ( GAME_WIDTH - (GAME_WIDTH/ZOOM_FACTOR) ) / 2 ) + (GAME_WIDTH/ZOOM_FACTOR),
+            y: ( GAME_HEIGHT - (GAME_HEIGHT/ZOOM_FACTOR) ) / 2,
         },
-
-    },
-    scene: [
-        new ManufacturerScene(SHARED_CONFIG),
-        new PreloadScene(SHARED_CONFIG),
-        new MenuScene(SHARED_CONFIG),
-        new TableSelectionScene(SHARED_CONFIG),
-        new PlayScene(SHARED_CONFIG),
-        new PauseScene(SHARED_CONFIG),
-        new ConfirmScene(SHARED_CONFIG),
-        new GameCompleteScene(SHARED_CONFIG),
-        new OptionsScene(SHARED_CONFIG),
-        new CreditsScene(SHARED_CONFIG),
-        new TutorialScene(SHARED_CONFIG)
-    ],
-};
-
-new Phaser.Game(config);
+        bottomRight: {
+            x: ( ( GAME_WIDTH - (GAME_WIDTH/ZOOM_FACTOR) ) / 2 ) + (GAME_WIDTH/ZOOM_FACTOR),
+            y: ( (GAME_HEIGHT - (GAME_HEIGHT/ZOOM_FACTOR) ) / 2 ) + (GAME_HEIGHT/ZOOM_FACTOR),
+        },
+        debug: true
+    };
+    
+    const config= {
+        type: Phaser.CANVAS,
+        ...SHARED_CONFIG, 
+        parent: "gameWrapper",
+        backgroundColor: 0x21190,
+        transparent: true,
+        scale: {
+             mode: Phaser.Scale.Fit,
+             autoCenter: Phaser.Scale.CENTER_BOTH,
+        },
+        pixelArt: false, 
+        physics:{
+            default: 'arcade',
+            arcade:{
+                debug: SHARED_CONFIG.debug,
+            },
+            matter:{
+                debug: SHARED_CONFIG.debug,
+            },
+    
+        },
+        scene: [
+            new ManufacturerScene(SHARED_CONFIG),
+            new PreloadScene(SHARED_CONFIG),
+            new MenuScene(SHARED_CONFIG),
+            new TableSelectionScene(SHARED_CONFIG),
+            new PlayScene(SHARED_CONFIG),
+            new PauseScene(SHARED_CONFIG),
+            new ConfirmScene(SHARED_CONFIG),
+            new GameCompleteScene(SHARED_CONFIG),
+            new OptionsScene(SHARED_CONFIG),
+            new CreditsScene(SHARED_CONFIG),
+            new TutorialScene(SHARED_CONFIG)
+        ],
+    };
+    
+    new Phaser.Game(config);
+});
